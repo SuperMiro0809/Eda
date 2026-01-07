9f8742c2737ffe8e9520d6b43a5f0f476983e876
Miroslav Balev <m.balev@editcreation.com>
2026-01-07 16:11:33 +0200

    add locales and multi language support

---
+++ client/src/locales/localization-provider.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import 'dayjs/locale/en';
+
+import dayjs from 'dayjs';
+
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider as Provider } from '@mui/x-date-pickers/LocalizationProvider';
+
+import { useTranslate } from './use-locales';
+
+// ----------------------------------------------------------------------
+
+export function LocalizationProvider({ children }) {
+  const { currentLang } = useTranslate();
+
+  dayjs.locale(currentLang.adapterLocale);
+
+  return (
+    <Provider dateAdapter={AdapterDayjs} adapterLocale={currentLang.adapterLocale}>
+      {children}
+    </Provider>
+  );
+}
